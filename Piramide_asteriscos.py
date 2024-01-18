fb82a9e8ed9ea1f144acc2f927c4990c348145b8
Valdi183 <vvaldcal@myuax.com>
2024-01-18 11:45:30 +0100

    Añadido archivo que contiene el código del primer ejercicio

---
+++ Piramide_asteriscos.py
@@ -0,0 +1,23 @@
+"""
+Este código contiene el programa que genera una pirámide de asteriscos, según el número
+que pida el usuario
+"""
+numero_usuario = 0
+asteriscos = []
+
+def preguntar_usuario(numero_usuario):
+    while True:
+        try:
+            numero_usuario = int(input("Introduzca un número entero positivo menor que 8: "))
+            if numero_usuario > 8 or numero_usuario < 1:
+                print("Introduzca un numero válido")
+        except:
+            ValueError
+        return numero_usuario
+
+def generar_pirámide(numero_usuario,asteriscos):
+    for element in numero_usuario:
+        print(asteriscos)
+
+preguntar_usuario(numero_usuario)
+generar_pirámide(numero_usuario,asteriscos)
